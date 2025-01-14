6942abb06d8c9db2db8a9594f1966c513b400d59
Izfaha <rasberry006@gmail.com>
2025-01-15 00:11:34 +0700

    three main files c++ project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "header.h"
+
+int main(){
+    
+    int iteration = 0;
+    dataLib information[100]; // '100' declare the number of array | // if array has been declared, no need to declare anymore 
+    headerScript();
+    inputBookData(information, iteration); // in this function . "information[]" = is incorrect
+
+    return 0;
+}
+
+
